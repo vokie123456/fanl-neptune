46b451db5a76c919d5a067dcf55a5f3778052b04
fanling <862844083@qq.com>
2019-02-22 18:35:36 +0800

    修改信息，添加引用

--- src/main/java/com/fanling/neptune/NeptuneApplication.java
+++ src/main/java/com/fanling/neptune/NeptuneApplication.java
@@ -1,12 +1,13 @@
 package com.fanling.neptune;
 
+import com.fanling.neptune.config.NeptuneConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({NeptuneConfig.class})
 @MapperScan("com.fanling.neptune.mapper")
 public class NeptuneApplication {
 
